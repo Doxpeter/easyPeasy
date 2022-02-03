4fc09a258ff4c4685ca28ac0eb575e27bd76c6e3
Doxper <youngqi.jp@gmail.com>
2022-02-03 23:14:48 +0900

    💡  ボタンをコンポーネント化

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+export function Button() {
+  return (
+    <div className="md:w-1/2 lg:w-3/5 w-full m-auto">
+      <button className="shadow-lg w-full py-5  bg-orange-500 text-bold font-Raleway text-white rounded-md  hover:bg-orange-300 hover:shadow-sm hover:translate-y-0.5 transform transition">
+        GENERATE PASSWORD
+      </button>
+    </div>
+  );
+}
